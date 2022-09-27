be10030c616d4c7653cca5d646f26719bf9d33cf
Eruck Emmanuel <eruck@paymentology.com>
2022-09-27 12:55:00 +0100

    Added unit test for api

--- hello-world/tests/unit/test-handler.js
+++ hello-world/tests/unit/test-handler.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const app = require('../../app.js');
+const axios = require('axios');
 const chai = require('chai');
 const expect = chai.expect;
 var event, context;
@@ -18,5 +19,13 @@ describe('Tests index', function () {
         expect(response).to.be.an('object');
         expect(response.message).to.be.equal("hello world");
         // expect(response.location).to.be.an("string");
+    });
+    it("verifies successful response from through API", async () => {
+        const url = "http://localhost:3000/hello";
+
+        let response = await axios.get(url);
+
+        expect(response.status).to.be.equal(200);
+        expect(response.data.message).to.be.equal("hello world");
     });
 });
